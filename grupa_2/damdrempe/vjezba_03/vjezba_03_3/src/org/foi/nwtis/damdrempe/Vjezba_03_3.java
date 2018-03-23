6357511ece361d069c3148be8ed9ef59b95f3146
Damir Drempetic <ddrempe@gmail.com>
2018-03-23 17:12:02 +0100

    Inicijalne ispravke na vjezba_03_3

--- grupa_2/damdrempe/vjezba_03/vjezba_03_3/src/org/foi/nwtis/damdrempe/Vjezba_03_3.java
+++ grupa_2/damdrempe/vjezba_03/vjezba_03_3/src/org/foi/nwtis/damdrempe/Vjezba_03_3.java
@@ -1,5 +1,11 @@
 package org.foi.nwtis.damdrempe;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.foi.nwtis.damdrempe.konfiguracije.KonfiguracijaApstraktna;
+import org.foi.nwtis.damdrempe.konfiguracije.NeispravnaKonfiguracija;
+import org.foi.nwtis.damdrempe.konfiguracije.NemaKonfiguracije;
+
 public class Vjezba_03_3 {
 
     public static void main(String[] args) {
@@ -8,7 +14,11 @@ public class Vjezba_03_3 {
             return;
         }
         
-        KonfiguracijaApstraktna.kreirajKonfiguraciju(args[0]);
+        try {
+            KonfiguracijaApstraktna.kreirajKonfiguraciju(args[0]);
+        } catch (NemaKonfiguracije | NeispravnaKonfiguracija ex) {
+            Logger.getLogger(Vjezba_03_3.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
     
 }
